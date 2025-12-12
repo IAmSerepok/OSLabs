c413102f92124c3a04cc61db8d67e3c72c5a161c
Serepok <gromyko-04@bk.ru>
2025-12-12 21:59:45 +1000

    lab4

--- lab4/posix_test.sh
+++ lab4/posix_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-socat -d -d pty,raw,echo=0,link=/tmp/virtual_com1 pty,raw,echo=0,link=/tmp/virtual_com2
-./build/output/logger /tmp/virtual_com1
-./build/output/emulator /tmp/virtual_com2
+x-terminal-emulator -e "socat -d -d pty,raw,echo=0,link=/tmp/virtual_com1 pty,raw,echo=0,link=/tmp/virtual_com2"
+sleep 1
+x-terminal-emulator -e "./build/output/logger /tmp/virtual_com1"
+x-terminal-emulator -e "./build/output/emulator /tmp/virtual_com2"
