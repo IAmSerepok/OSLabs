60031f9a9ad150cb0a745347409adf0bc2a0e489
Serepok <gromyko-04@bk.ru>
2025-12-12 21:53:39 +1000

    lab4

--- lab4/posix_test.sh
+++ lab4/posix_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-socat -d -d pty,raw,echo=0,link=/tmp/virtual_com1 pty,raw,echo=0,link=/tmp/virtual_com2 2>/tmp/socat
+socat -d -d pty,raw,echo=0,link=/tmp/virtual_com1 pty,raw,echo=0,link=/tmp/virtual_com2
 ./build/output/logger /tmp/virtual_com1
 ./build/output/emulator /tmp/virtual_com2
